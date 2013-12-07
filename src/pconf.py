dee4a429b535a6701c6768f6d2c8885f420edbc7
Mattias Andrée <maandree@operamail.com>
2013-12-07 19:55:30 +0100

    remove I_USE_AUDIT from pconf as enabling it creates an dependency
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/pconf.py
+++ src/pconf.py
@@ -49,7 +49,6 @@ args.append(_bool('shared',     'y'))
 args.append(_bool('static',     'n'))
 args.append(_bool('largefiles', 'y'))
 args.append(_bool('valgrind',   'n'))
-args.append(_bool('audit',      'y'))
 args.append(_bool('selinux',    'n'))
 args.append(_bool('nls',        'y'))
 args += extra_args
