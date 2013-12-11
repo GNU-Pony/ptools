ac1db56f8acc84580c2b8fbda9a6a09fb01e7639
Mattias Andrée <maandree@operamail.com>
2013-12-11 03:31:47 +0100

    add pgpp
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ src/pgpp.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+'''
+ptools – software package installation tools
+
+Copyright © 2013  Mattias Andrée (maandree@member.fsf.org)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+import sys
+import os
+
+try:
+    SPIKE_PATH = os.getenv('SPIKE_PATH')
+    sys.path.append('%s/src' % SPIKE_PATH)
+    from dragonsuite import *
+except:
+    print('Environment variable SPIKE_PATH has not been set correctly')
+    sys.exit(1)
+
+
+from fh import *
+
+args = ['gpp']
+for key in evald_dirs.keys():
+    args.append('-D')
+    args.append('%s=%s' % (key, evald_dirs[key]))
+args += extra_args
+
+execute(args)
+
